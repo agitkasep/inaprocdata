16f150cf22823f4ad90d0f577b7df70530d9c931
Cah Bagus Cerbon <agitkasep80@gmail.com>
2026-06-21 05:07:06 +0700

    Fix workflow and config files

--- Preview_Web/vite.config.js
+++ Preview_Web/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 'base' ini wajib agar aplikasi Anda tidak error saat dibuka di GitHub Pages
-  base: './', 
+  base: './',
   server: {
     port: 8000,
   },
