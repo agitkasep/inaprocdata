016ea0e2c371bae267bf60d5c1b6791fc3c88463
Cah Bagus Cerbon <agitkasep80@gmail.com>
2026-06-21 04:40:32 +0700

    Fix deployment path

--- Preview_Web/vite.config.js
+++ Preview_Web/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 'base' ini wajib agar aplikasi Anda tidak error saat dibuka di GitHub Pages
+  base: './', 
   server: {
     port: 8000,
   },
